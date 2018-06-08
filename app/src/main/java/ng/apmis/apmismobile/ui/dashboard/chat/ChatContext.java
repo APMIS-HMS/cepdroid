eefb288cb3d85eac83c3fb260f24135dbd6a1cb1
Thadeus Ajayi <ainojie@gmail.com>
2018-06-08 14:36:23 +0100

    Chat and add chats

--- app/src/main/java/ng/apmis/apmismobile/ui/dashboard/chat/ChatContext.java
+++ app/src/main/java/ng/apmis/apmismobile/ui/dashboard/chat/ChatContext.java
@@ -9,6 +9,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -37,20 +39,34 @@ public class ChatContext extends Fragment {
 
         chatContextAdapter = new ChatContextAdapter(getActivity());
 
-        existingChats.add(new Chats("akjsljfalsdjf;laksdfl;ajskljdf;alsjd;flakjsd;fjas;ldjf;lasjkflkjasdljfa;sjdflajksdfljasl;jkdfasjl;kdfjas;lkjdflajksd;fjasljfdals;jf;ajsdfjas;jd;fjasdjf;asdf", "me", R.drawable.ic_buy, new Date().getTime()));
-        existingChats.add(new Chats("This is Ikeja", "me", R.drawable.ic_diagnostic_report, new Date().getTime()));
-        existingChats.add(new Chats("This is Agege", "me", R.drawable.ic_pay_bills, new Date().getTime()));
-        existingChats.add(new Chats("This is London", "Other", R.drawable.ic_find, new Date().getTime()));
-        existingChats.add(new Chats("This is Lagos", "me", R.drawable.ic_buy, new Date().getTime()));
-        existingChats.add(new Chats("This is Fausets", "Other", R.drawable.ic_buy, new Date().getTime()));
+        existingChats.add(new Chats("Hi Thadeus.\nWhat do you want today?", "Other", R.drawable.ic_buy, new Date().getTime()));
+        existingChats.add(new Chats("I feel feverish and have a runny nose.\nWhat could be wrong with me?", "me", R.drawable.ic_diagnostic_report, new Date().getTime()));
+        existingChats.add(new Chats("Sorry about that...", "Other", R.drawable.ic_find, new Date().getTime()));
+        existingChats.add(new Chats("Are your visions blurry?", "Other", R.drawable.ic_buy, new Date().getTime()));
+        existingChats.add(new Chats("Yes", "me", R.drawable.ic_buy, new Date().getTime()));
 
         RecyclerView chatMessageRecycler = rootView.findViewById(R.id.chat_recycler);
+
+        EditText chatMessageEditText = rootView.findViewById(R.id.add_chat);
+
+        ImageView sendBtn = rootView.findViewById(R.id.send_chat);
 
         chatMessageRecycler.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
 
         chatMessageRecycler.setAdapter(chatContextAdapter);
 
         chatContextAdapter.setAllChats(existingChats);
+
+        chatMessageRecycler.smoothScrollToPosition(existingChats.size());
+
+        sendBtn.setOnClickListener((view) -> {
+            Chats oneChat = new Chats(chatMessageEditText.getText().toString(),"me", R.drawable.ic_buy, new Date().getTime());
+            ArrayList<Chats> chats = new ArrayList<>();
+            chats.add(oneChat);
+            chatContextAdapter.addChats(chats);
+            chatMessageRecycler.smoothScrollToPosition(chatContextAdapter.getItemCount());
+            chatMessageEditText.setText("");
+        });
 
         return rootView;
     }
